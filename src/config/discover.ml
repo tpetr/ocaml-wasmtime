cfd3539c9e633f62d5bae8fa450628c3f9e449e8
Tom Petr <tom@r2c.dev>
2026-02-13 10:47:37 -0500

    Factor out wasmtime lib into libwasmtime

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -3,10 +3,15 @@ module C = Configurator.V1
 let empty_flags = { C.Pkg_config.cflags = []; libs = [] }
 
 let resolve_path p =
-  if Filename.is_relative p then Filename.concat (Sys.getcwd ()) p else p
-
-let wasmtime_dir = ref ""
-let used_wasmtime_dir_fallback = ref false
+  if Filename.is_relative p then
+    (* Inside dune, cwd is the build dir; use DUNE_SOURCEROOT to resolve
+       paths relative to the project root where the user invoked dune. *)
+    let root = match Sys.getenv_opt "DUNE_SOURCEROOT" with
+      | Some r -> r
+      | None -> Sys.getcwd ()
+    in
+    Filename.concat root p
+  else p
 
 (* Find wasmtime installed via Homebrew (macOS) *)
 let homebrew_prefix () =
@@ -37,32 +42,18 @@ let wasmtime_flags () =
     let libs = [lib_file] in
     { C.Pkg_config.cflags; libs }
   in
-  (* Priority: LIBWASMTIME env > Homebrew > OPAM_SWITCH_PREFIX > --wasmtime-dir flag *)
+  (* Priority: LIBWASMTIME env > Homebrew > OPAM_SWITCH_PREFIX *)
   match Sys.getenv_opt "LIBWASMTIME" with
   | Some lib_dir -> config ~lib_dir
   | None ->
-    let from_brew = match homebrew_prefix () with
-      | Some prefix -> Some (config ~lib_dir:prefix)
-      | None -> None
-    in
-    let from_prefix = match from_brew with
-      | Some _ -> from_brew
-      | None ->
-        (match Sys.getenv_opt "OPAM_SWITCH_PREFIX" with
-         | Some prefix ->
-           let lib_dir = Filename.concat (Filename.concat prefix "lib") "libwasmtime" in
-           if Sys.file_exists lib_dir then Some (config ~lib_dir)
-           else None
-         | None -> None)
-    in
-    (match from_prefix with
-     | Some flags -> flags
-     | None ->
-       if !wasmtime_dir <> "" then begin
-         used_wasmtime_dir_fallback := true;
-         config ~lib_dir:!wasmtime_dir
-       end
-       else empty_flags)
+    match homebrew_prefix () with
+    | Some prefix -> config ~lib_dir:prefix
+    | None ->
+      match Sys.getenv_opt "OPAM_SWITCH_PREFIX" with
+      | Some prefix ->
+        let lib_dir = Filename.concat (Filename.concat prefix "lib") "libwasmtime" in
+        config ~lib_dir
+      | None -> empty_flags
 
 let system_libs () =
   match Sys.os_type with
@@ -80,35 +71,11 @@ let system_libs () =
 
 let () =
   C.main ~name:"wasmtime-config"
-    ~args:[("--wasmtime-dir", Arg.Set_string wasmtime_dir,
-            "DIR Path to wasmtime C API directory")]
     (fun _c ->
     let wasmtime_flags =
       try wasmtime_flags () with
       | _ -> empty_flags
     in
     let sys_libs = try system_libs () with _ -> ["-lm"] in
-    (* When building as an opam package and falling through to --wasmtime-dir,
-       the resolved path points into the ephemeral opam build directory which
-       won't exist after installation. Override c_library_flags to use the
-       install path ($OPAM_SWITCH_PREFIX/lib/libwasmtime/) so that downstream
-       packages can find libwasmtime at its installed location. *)
-    let libs =
-      if !used_wasmtime_dir_fallback
-         && Sys.getenv_opt "OPAM_PACKAGE_NAME" <> None then
-        match Sys.getenv_opt "OPAM_SWITCH_PREFIX" with
-        | Some prefix ->
-          let install_lib_dir =
-            List.fold_left Filename.concat prefix ["lib"; "libwasmtime"; "lib"]
-          in
-          let lib_name = match Sys.os_type with
-            | "Win32" | "Cygwin" -> "wasmtime.dll.lib"
-            | _ -> "libwasmtime.a"
-          in
-          [Filename.concat install_lib_dir lib_name]
-        | None -> wasmtime_flags.libs
-      else
-        wasmtime_flags.libs
-    in
     C.Flags.write_sexp "c_flags.sexp" wasmtime_flags.cflags;
-    C.Flags.write_sexp "c_library_flags.sexp" (libs @ sys_libs))
+    C.Flags.write_sexp "c_library_flags.sexp" (wasmtime_flags.libs @ sys_libs))
